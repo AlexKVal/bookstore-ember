32a20e33346b8d477b38017207e9e043c150daa5
AlexKVal <alexkval@gmail.com>
2016-03-06 22:15:14 +0200

    add "checkout-modal" component

---
+++ app/components/checkout-modal.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    close() { return this.sendAction('close'); }
+  }
+});
--- app/routes/books.js
+++ app/routes/books.js
@@ -3,5 +3,23 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('book');
+  },
+
+  actions: {
+    openCheckoutModal(book) {
+      return this.render('components/checkout-modal', {
+        outlet: 'modal',
+        into: 'application',
+        model: book,
+        controller: 'application'
+      });
+    },
+
+    closeCheckoutModal() {
+      return this.disconnectOutlet({
+        outlet: 'modal',
+        parentView: 'application'
+      });
+    }
   }
 });
